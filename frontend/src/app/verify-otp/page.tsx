abd5dc294f8041b8c587a258d1b37097c3618673
YogeshK05 <108150135+YogeshK05@users.noreply.github.com>
2025-02-22 13:11:52 +0530

    Added suspense in verify-otp.

--- frontend/src/app/verify-otp/page.tsx
+++ frontend/src/app/verify-otp/page.tsx
@@ -1,32 +1,34 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import OtpForm from '@/components/OtpForm';
+import { Suspense, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import OtpForm from "@/components/OtpForm";
 
 export default function VerifyOtpPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [email, setEmail] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
 
   useEffect(() => {
-    const emailParam = searchParams.get('email');
+    const emailParam = searchParams.get("email");
     if (!emailParam) {
-      router.push('/login');
+      router.push("/login");
       return;
     }
     setEmail(emailParam);
   }, [router, searchParams]);
 
   return (
-    <div className="max-w-md mx-auto">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-6">Verify OTP</h1>
-        <p className="text-gray-600 mb-6">
-          Please enter the 6-digit code sent to your email address {email}
-        </p>
-        <OtpForm email={email} />
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="max-w-md mx-auto">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-bold mb-6">Verify OTP</h1>
+          <p className="text-gray-600 mb-6">
+            Please enter the 6-digit code sent to your email address {email}
+          </p>
+          <OtpForm email={email} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
